4415509885d35bde310a3f94c2120e1a28549f21
Aman <amanxmhd@gmail.com>
2025-06-06 17:09:55 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:avatar/core/themes/light/light_theme_data.dart';
 import 'package:avatar/views/map/map_scree_view.dart';
+import 'package:avatar/views/purchase/widgets/submit_request.dart';
 import 'package:avatar/views/purchase/widgets/transaction_history.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
       // If user is logged in, navigate to home page
       // If user is not logged in, navigate to select role page
       //home: SelectRoleView(),
-      home: TransactionHistory(),
+      home: SubmitRequestPage(),
     );
   }
 }
