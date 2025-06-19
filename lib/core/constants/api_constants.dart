338886cebe1672134b9102e5d185d28eb5345820
jaylodha10 <jaylodha2003@gmail.com>
2025-06-19 15:32:16 +0530

    added login api

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'http://122.170.111.109:6184';
+
+  static const String loginEndpoint = '$baseUrl/api/login_view/';
+
+  static const String registerEndpoint = '$baseUrl/api/register_user/';
+}
