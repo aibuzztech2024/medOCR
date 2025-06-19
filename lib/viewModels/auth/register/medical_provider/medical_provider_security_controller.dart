a387fc8720bbd7b001ba693fdde06603c6e86f1f
Anderson Correya <bervincorreya@gmail.com>
2025-06-19 16:02:55 +0530

    upto now

--- lib/viewModels/auth/register/medical_provider/medical_provider_security_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
-class MedicalSecurityController extends GetxController {
-  var usernameController = TextEditingController();
-  var passwordController = TextEditingController();
-  var confirmPasswordController = TextEditingController();
-}
